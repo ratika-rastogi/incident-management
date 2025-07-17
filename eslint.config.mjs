f05124e9c736ddc82d66e6c8b003f6b2a8000b2f
RATIKA RASTOGI <ratikarastogi1996@gmail.com>
2025-07-17 12:51:27 +0000

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const ui5TestsConfig = {
     "no-unused-vars": [
       "error",
       {
-        argsIgnorePattern: "When|Then|Given",
+        argsIgnorePattern: "When|Given",
       },
     ],
   },
